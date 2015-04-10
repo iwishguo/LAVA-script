2a8b8990736e9e173f41cbbcb0a0317b7d4ce951
Heyi Guo <heyi.guo@linaro.org>
2015-04-10 08:56:43 +0800

    Try redirect build log to accelerate process

--- template_replace.pl
+++ template_replace.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 
 my $TEMPLATE = <<EOF
-       - lava-test-case <TEMP>-debug --shell ../uefi-tools/uefi-build.sh -b DEBUG <TEMP>
+       - lava-test-case <TEMP>-debug --shell ../uefi-tools/uefi-build.sh -b DEBUG <TEMP> > build_log
+       - tail -n 50 build_log
        - rm -rf Build
-       - lava-test-case <TEMP>-release --shell ../uefi-tools/uefi-build.sh -b RELEASE <TEMP> 
+       - lava-test-case <TEMP>-release --shell ../uefi-tools/uefi-build.sh -b RELEASE <TEMP> > build_log 
+       - tail -n 50 build_log
        - rm -rf Build
 EOF
 ;
