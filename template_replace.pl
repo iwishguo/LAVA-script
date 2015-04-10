ab5d8022667b405b2cca4f7ba21285c0d2affdc5
Heyi Guo <heyi.guo@linaro.org>
2015-04-10 10:00:35 +0800

    Redirect stdout and stderr together to make it easy to read

--- template_replace.pl
+++ template_replace.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 
 my $TEMPLATE = <<EOF
-       - lava-test-case <TEMP>-debug --shell "../uefi-tools/uefi-build.sh -b DEBUG <TEMP> > build_log"
-       - tail -n 50 build_log
+       - lava-test-case <TEMP>-debug --shell "../uefi-tools/uefi-build.sh -b DEBUG <TEMP> >& build_log"
+       - tail -n 100 build_log
        - rm -rf Build
-       - lava-test-case <TEMP>-release --shell "../uefi-tools/uefi-build.sh -b RELEASE <TEMP> > build_log"
-       - tail -n 50 build_log
+       - lava-test-case <TEMP>-release --shell "../uefi-tools/uefi-build.sh -b RELEASE <TEMP> >& build_log"
+       - tail -n 100 build_log
        - rm -rf Build
 EOF
 ;
