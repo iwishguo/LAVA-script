7a9ff6b26210b1f7a0231e8074a9693788015af9
Heyi Guo <heyi.guo@linaro.org>
2015-04-10 12:45:38 +0800

    add gcc 4.7 4.8

--- template_replace.pl
+++ template_replace.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 
 my $TEMPLATE = <<EOF
-       - lava-test-case <TEMP>-debug --shell "../uefi-tools/uefi-build.sh -b DEBUG <TEMP> > build_log"
+       - lava-test-case gcc-\${GCC_REV}-<TEMP>-debug --shell "../uefi-tools/uefi-build.sh -b DEBUG <TEMP> > build_log"
        - tail -n 50 build_log
        - rm -rf Build
-       - lava-test-case <TEMP>-release --shell "../uefi-tools/uefi-build.sh -b RELEASE <TEMP> > build_log"
+       - lava-test-case gcc-\${GCC_REV}-<TEMP>-release --shell "../uefi-tools/uefi-build.sh -b RELEASE <TEMP> > build_log"
        - tail -n 50 build_log
        - rm -rf Build
 EOF
